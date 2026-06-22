b23725bba6c436388b930fb16f3790468712a082
Gravity Denim <gravitydenim@gmail.com>
2026-06-22 08:34:46 -0500

    chore: force remove .next from git

--- .next/server/pages/_error.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_error.js")
-R.c("server/chunks/ssr/[externals]_next_dist_shared_lib_no-fallback-error_external_0r3u29k.js")
-R.c("server/chunks/ssr/node_modules_0luqkzx._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1n4gtax._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1a16hm-._.js")
-R.c("server/chunks/ssr/node_modules_next_1_o5zxs._.js")
-R.m(25695)
-module.exports=R.m(25695).exports
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dlcVBbaslPOwN7rkO1PP/VyO6qiiIlI2pnWY7LXlH3Y=\"\n}"
